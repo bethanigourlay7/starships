95a97991fdc35fff770b566309a73dcae03f9f5c
Sam Millar <samillar94@outlook.com>
2023-05-15 12:45:59 +0100

    nodifieddddd

--- app.js
+++ app.js
@@ -27,7 +27,13 @@ app.use((req, res, next) => {
 }); /// exposes session to ejs templates
 
 /// data
-let employeedata = {};
+let employeeData = [
+    {first_name: "Seamus", last_name: "McBride",salary: 30000, role : "Ejit", employee_number: 42069},
+    {first_name: "Sam", last_name: "Millar",salary: 40000, role : "Tech Lead", employee_number: 420670},
+    {first_name: "Paula", last_name: "Sankiewicz",salary: 50000, role : "UX Lead", employee_number: 42071},
+    {first_name: "Bethani", last_name: "Gourlay",salary: 60000, role : "UI Lead", employee_number: 42072},
+    {first_name: "Dylan", last_name: "Robinson",salary: 70000, role : "Database Manager", employee_number: 42073},
+];
 
 /// routes
 
@@ -35,7 +41,18 @@ app.get('/', (req, res)=> {
 
     res.render('home', {
         title: 'Employees', 
-        employeedata
+        employeeData
+    }); 
+
+});
+
+app.get('/viewemployee', (req, res)=> { 
+
+    let index = req.index;
+
+    res.render('home', {
+        title: 'Employees', 
+        employee: employeeData[index]
     }); 
 
 });
