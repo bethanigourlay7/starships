c8f0bd7ac1c8ee0765249dfd6a86a898bd3c08d8
bethanigourlay7 <gourlaybethani@gmail.com>
2023-05-15 14:05:32 +0100

    Add job roles

---
+++ job_roles.js
@@ -0,0 +1,40 @@
+
+/**
+ * Will be inserted into dropdown list 
+ * @role_id - unique id per role
+ * @role - name of role
+ * @
+ */
+
+let roles = [
+    {role_id: 1, role: "Tech Lead"},
+    
+    {role_id: 2, role: "Senior Security Engineer"},
+    
+    {role_id: 3, role: "Software Engineer"},
+    
+    {role_id: 4, role: "UX Lead"},
+    
+    {role_id: 5, role: "Data Archicect"},
+    
+    {role_id: 6, role: "UI Lead"},
+    
+    {role_id: 7, role: "Workday Consultant"}
+]
+// let roles = [
+
+//     {role_id: 1, role: "Tech Lead", category: "Workday", time: "Full time", location: "Belfast"},
+    
+//     {role_id: 2, role: "Senior Security Engineer", category: "Cyber Security", time: "Full time", location: "Belfast"},
+    
+//     {role_id: 3, role: "", category: "Data and AI", time: "Full time", location: "London"},
+    
+//     {role_id: 4, role: "UX Lead", category: "UX/UI Design", time: "Full time", location: "London"},
+    
+//     {role_id: 5, role: "Data Archicect", category: "Data and AI", time: "Full time", location: "Toronto"},
+    
+//     {role_id: 6, role: "UI Lead", category:  "UX/UI Design", time: "Full time", location: "Birmingham"},
+    
+//     {role_id: 7, role: "Workday Consultant", category: "Workday", time: "Full time", location: "Dublin"},
+
+// ]
