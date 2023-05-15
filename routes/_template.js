5bd7ea0c1b35d27b59f9a26701d1d8b960ef87ee
Sam Millar <samillar94@outlook.com>
2023-05-15 12:07:41 +0100

    Routes

---
+++ routes/_template.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+router.get('/', (req, res)=> { 
+
+    // let templateQ = `SELECT * FROM table`;
+
+});
+
+router.post('/', (req, res)=> { 
+
+    // let templateQ = `SELECT * FROM table`;
+
+});
+
+module.exports = router;
