38d7c6e69df50d9e7c6edfae4a419696b2055ec2
SeamusMacB <aaron3112@hotmail.co.uk>
2023-05-15 11:54:42 +0100

    employee test data

---
+++ employees.js
@@ -0,0 +1,19 @@
+
+
+// Employee Structure
+
+/*
+Essential details:
+Name, address, Salary, Role, Employee Number
+
+Additional Details:
+Location, Team, Reports to/Manager
+*/
+
+employees = [
+    {first_name: "Seamus", last_name: "McBride",salary: 30000, role : "Ejit", employee_number: 42069},
+    {first_name: "Sam", last_name: "Millar",salary: 40000, role : "Tech Lead", employee_number: 420670},
+    {first_name: "Paula", last_name: "Sankiewicz",salary: 50000, role : "UX Lead", employee_number: 42071},
+    {first_name: "Bethani", last_name: "Gourlay",salary: 60000, role : "UI Lead", employee_number: 42072},
+    {first_name: "Dylan", last_name: "Robinson",salary: 70000, role : "Database Manager", employee_number: 42073},
+]
