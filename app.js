ba1f709ab7fe390d9c5c406059ad1d50e10fe339
Sam Millar <samillar94@outlook.com>
2023-05-15 12:53:14 +0100

    works now lol

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /// imports
 const express = require("express");
-const sessions = require('express-session');
+// const sessions = require('express-session');
 const fs = require('fs');
 
 /// app and ports 
@@ -10,29 +10,29 @@ const APP_PORT = process.env.APP_PORT || 3000;
 /// their middleware 
 app.use(express.static('static'));
 app.set('view engine', 'ejs');
-app.use(sessions({
-    secret: process.env.SESSIONS_SECRET,
-    saveUninitialized: false,
-    resave: false
-    })
-);
+// app.use(sessions({
+//     secret: process.env.SESSIONS_SECRET,
+//     saveUninitialized: false,
+//     resave: false
+//     })
+// );
 app.use(express.urlencoded({extended: true})); 
 
 /// my middleware
-app.use((req, res, next) => {
-    res.locals.query = req.query;
-    res.locals.user_id = req.session.user_id;
-    res.locals.member = req.session.sess_valid;
-    next();
-}); /// exposes session to ejs templates
+// app.use((req, res, next) => {
+//     res.locals.query = req.query;
+//     res.locals.user_id = req.session.user_id;
+//     res.locals.member = req.session.sess_valid;
+//     next();
+// }); /// exposes session to ejs templates
 
 /// data
 let employeeData = [
-    {first_name: "Seamus", last_name: "McBride",salary: 30000, role : "Ejit", employee_number: 42069},
-    {first_name: "Sam", last_name: "Millar",salary: 40000, role : "Tech Lead", employee_number: 420670},
-    {first_name: "Paula", last_name: "Sankiewicz",salary: 50000, role : "UX Lead", employee_number: 42071},
-    {first_name: "Bethani", last_name: "Gourlay",salary: 60000, role : "UI Lead", employee_number: 42072},
-    {first_name: "Dylan", last_name: "Robinson",salary: 70000, role : "Database Manager", employee_number: 42073},
+    {first_name: "Seamus", last_name: "McBride",salary: 30000, role : "Ejit", address: "here", employee_number: 42069},
+    {first_name: "Sam", last_name: "Millar",salary: 40000, role : "Tech Lead", address: "here", employee_number: 420670},
+    {first_name: "Paula", last_name: "Sankiewicz",salary: 50000, role : "UX Lead", address: "here", employee_number: 42071},
+    {first_name: "Bethani", last_name: "Gourlay",salary: 60000, role : "UI Lead", address: "here", employee_number: 42072},
+    {first_name: "Dylan", last_name: "Robinson",salary: 70000, role : "Database Manager", address: "here", employee_number: 42073},
 ];
 
 /// routes
