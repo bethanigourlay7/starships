4f309a1986c82f7a4b9d07272dc5e48ba8af4f0a
Paula Santkiewicz <santkiewicz.paula@gmail.com>
2023-05-16 13:21:43 +0100

    Update to formatting

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ let roles = [
 let employeeData = [
     { first_name: "Seamus", last_name: "McBride", salary: 30000, role: "Ejit", address: "here", employee_number: 42069 },
     { first_name: "Sam", last_name: "Millar", salary: 40000, role: "Tech Lead", address: "here", employee_number: 420670 },
-    { first_name: "Paula", last_name: "Sankiewicz", salary: 50000, role: "UX Lead", address: "here", employee_number: 42071 },
+    { first_name: "Paula", last_name: "Santkiewicz", salary: 50000, role: "UX Lead", address: "here", employee_number: 42071 },
     { first_name: "Bethani", last_name: "Gourlay", salary: 60000, role: "UI Lead", address: "here", employee_number: 42072 },
     { first_name: "Dylan", last_name: "Robinson", salary: 70000, role: "Database Manager", address: "here", employee_number: 42073 },
 ];
@@ -117,8 +117,9 @@ app.post('/edit', (req, res) => {
     let employee = employeeData.filter((employee) => {
         return employee.employee_number.toString() === employeeNum;
     });
-    let index = employeeData.findIndex((emp) =>  {
-       return emp.employee_number.toString() === employeeNum});
+    let index = employeeData.findIndex((emp) => {
+        return emp.employee_number.toString() === employeeNum
+    });
 
     if (index !== -1) {
         employeeData[index].salary = req.body.employeeSalary;
@@ -128,7 +129,7 @@ app.post('/edit', (req, res) => {
         employeeData[index].role = req.body.jobTitle;
 
     }
-    
+
     // res.render('edit', {
     //     title: 'Edit employees', roles, employee, employeeNum
     // });
